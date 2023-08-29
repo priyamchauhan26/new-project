4fee1bfad7d4226acb99980a8ce97c1c28e740b0
Priyam <priyamchauhan26@gmail.com>
2023-08-29 22:06:37 +0530

    Product Form Completed

---
+++ src/app/service/product/product.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Product } from 'src/app/model/Product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  
+
+  constructor(private http:HttpClient) { }
+
+  addproduct(addproductform:any,product:Product){
+    product=addproductform.value;
+    return this.http.post<any>("",product)
+
+    
+  }
+}
