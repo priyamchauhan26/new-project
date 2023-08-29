a7513cc4fa70b715f06a849cb79512978f19f1ed
unknown <vimlesh@mygetepay.com>
2023-08-30 01:00:52 +0530

    subcat

--- src/app/addsubcategory/addsubcategory.component.ts
+++ src/app/addsubcategory/addsubcategory.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MerchantserviceService } from '../merchantservice.service';
+import { Router } from '@angular/router';
+import { tick } from '@angular/core/testing';
 
 @Component({
   selector: 'app-addsubcategory',
@@ -6,5 +9,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./addsubcategory.component.css']
 })
 export class AddsubcategoryComponent {
+  public productcatlist:any
+  selectcatname:any
+
+  public subcatlist:any
+  public  subcategory={
+    pcid:'',
+    subcategory:'',
+    desciption:''
+   }
+
+
+  categogory:any
+  http: any;
+
+  constructor(private merchanservice :MerchantserviceService,private router:Router)
+
+  {
+
+    this.merchanservice.getcategory().subscribe((data)=>
+    {
+      console.warn("data",data);
+      this.productcatlist=data;
+    }
+    );
+  }
+
+  ngOnInit(): void {
+       
+  }
+
+  
+ changecatname(e:any)
+ {
+  console.warn(e.target.value);
+  this.selectcatname=e.target.value
+
+  console.log(this.selectcatname);
+ }
+
+ formSubmit(){
+
+  this.subcategory.pcid  =this.selectcatname
+  this.merchanservice.addsubcat(this.subcategory).subscribe((data:any) =>
+    {
+      console.log(data);
+ });
+}
 
 }
--- src/app/merchantservice.service.ts
+++ src/app/merchantservice.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,20 @@ export class MerchantserviceService {
        return this.http.post('http://localhost:8082/add_prod_cat',data)
     }
 
-    
-   
+    addsubcat(data:any)
+    {
+       return this.http.post('http://localhost:8082/add_prod_subcat',data)
+    }
+
+    getProductSubcat(pcid: string): Observable<any> {
+      const apiUrl = 'http://localhost:8082/getproductsubcat';
+  
+      return this.http.get(apiUrl, { params: { pcid: pcid } });
+    }
+ 
+
+    getcategory()
+    {
+     return this.http.get('http://localhost:8082/getproductcat')
+    }
 }
